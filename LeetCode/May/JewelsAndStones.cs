842608d2bdfc031116b3f6b5de9be735a3cd7583
Harshit Gindra <rr221100@prscoad.com>
2020-08-10 11:59:48 -0500

    May Jewels And Stones Test

---
+++ LeetCode/May/JewelsAndStones.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.May
+{
+    public class JewelsAndStonesTest
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            int sum = 0;
+          
+            foreach (var item in J)
+            {
+                sum += S.Count(x => x == item);
+            }
+
+            return sum;
+        }
+    }
+}
