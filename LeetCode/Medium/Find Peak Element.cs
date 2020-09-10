6796906b042e4a1a1b757acbcb27499af6ac3842
Harshit Gindra <harshitgindra@gmail.com>
2020-09-10 00:11:53 -0500

    Medium challenge Find Peak Element  https://leetcode.com/problems/find-peak-element/

---
+++ LeetCode/Medium/Find Peak Element.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    class Find_Peak_Element
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    if (nums.Length > i + 1)
+                    {
+                        if (nums[i] > nums[i + 1])
+                        {
+                            return i;
+                        }
+                    }
+                    else
+                    {
+                        return i;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/find-peak-element/")]
+        [Category("Medium")]
+        [Category("Leetcode")]
+        [Category("Find Peak Element")]
+        [TestCaseSource("Input")]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = FindPeakElement(item.Input);
+            Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input
+        {
+            get
+            {
+                return new List<(int Output, int[] Input)>()
+                {
+                    (1, new int[] {1,2,1,3,5,6,4}),
+                    (2, new int[] {1,2,3,1}),
+                };
+            }
+        }
+    }
+}
