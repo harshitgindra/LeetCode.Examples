ca2569546a5174e559eaae93823d527251a40044
Harshit Gindra <harshitgindra@gmail.com>
2020-09-10 21:42:48 -0500

    https://leetcode.com/problems/missing-number/

--- LeetCode/Easy/Missing Number.cs
+++ LeetCode/Easy/Missing Number.cs
@@ -8,6 +8,26 @@ namespace LeetCode.Easy
 {
     public class Missing_Number
     {
-        
+        /// <summary>
+        /// https://leetcode.com/problems/missing-number/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int MissingNumber(int[] nums)
+        {
+            var hSet = nums.ToHashSet();
+            for(int i = 0; i< Int32.MaxValue; i++)
+            {
+                if (!hSet.Contains(i))
+                {
+                    return i;
+                }
+                else
+                {
+                    hSet.Remove(i);
+                }
+            }
+            return Int32.MaxValue;
+        }
     }
 }
