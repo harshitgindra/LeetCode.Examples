28a385f253ac4d64c31e3e244a14f1d2de171885
Harshit Gindra <harshitgindra@gmail.com>
2020-09-12 13:22:02 -0500

    https://leetcode.com/problems/product-of-array-except-self/

---
+++ LeetCode/Hard/Non-negative Integers without Consecutive Ones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace LeetCode.Hard
+{
+    class Non_negative_Integers_without_Consecutive_Ones
+    {
+        public int FindIntegers(int num)
+        {
+            int returnValue = num;
+            for (int i = 0; i <= num; i++)
+            {
+                string binary = Convert.ToString(i, 2);
+                if (binary.Contains("11"))
+                {
+                    returnValue--;
+                }
+            }
+
+            return Enumerable.Range(0,num).Count(x=>!Convert.ToString(x, 2).Contains("11"));
+
+            return returnValue;
+        }
+    }
+}
---
+++ LeetCode/Medium/House Robber III.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    public class House_Robber_III
+    {
+        public int Rob(TreeNode root)
+        {
+            var result = StartRobbing(root, 0, new SortedDictionary<int, int>());
+            return Rob(result.Values.ToArray(), 0, result.Count);
+        }
+
+        public int Rob(int[] nums, int start, int end)
+        {
+            int previousLevel1 = 0;
+            int previousLevel2 = 0;
+            int current = 0;
+            for (int i = start; i < end; i++)
+            {
+                current = Math.Max(nums[i] + previousLevel2, previousLevel1);
+                previousLevel2 = previousLevel1;
+                previousLevel1 = current;
+            }
+            return current;
+        }
+
+        private SortedDictionary<int, int> StartRobbing(TreeNode node, int level, SortedDictionary<int, int> result)
+        {
+            if (node != null)
+            {
+                if (result.ContainsKey(level))
+                {
+                    result[level] = result[level] + node.val;
+                }
+                else
+                {
+                    result.Add(level, node.val);
+                }
+
+                result = StartRobbing(node.left, level + 1, result);
+                result = StartRobbing(node.right, level + 1, result);
+            }
+
+            return result;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/house-robber-iii/")]
+        [Category("Medium")]
+        [Category("Leetcode")]
+        [Category("House Robber 3")]
+        public void Test1()
+        {
+            var response = Rob(new TreeNode(4, new TreeNode(1, new TreeNode(2, new TreeNode(3)))));
+        }
+    }
+}
