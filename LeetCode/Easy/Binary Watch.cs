225227ad232f9df114d7eb9c455ea24054ef2094
Harshit Gindra <harshitgindra@gmail.com>
2020-09-13 00:02:25 -0500

    https://leetcode.com/problems/binary-watch/

---
+++ LeetCode/Easy/Binary Watch.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    public class BinaryWatch
+    {
+        public IList<string> ReadBinaryWatch(int num)
+        {
+            List<string> result = new List<string>();
+
+            if (num <= 10)
+            {
+                Read(num, 0, new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, result);
+            }
+
+            return result;
+        }
+
+        private void Read(int num, int startIndex, int[] binaryText, List<string> result)
+        {
+            if (binaryText.Count(x => x == 1) == num)
+            {
+                int hour = Convert.ToInt32($"{binaryText[0]}{binaryText[1]}{binaryText[2]}{binaryText[3]}", 2);
+
+                if (hour < 12)
+                {
+                    int min = Convert.ToInt32($"{binaryText[4]}{binaryText[5]}{binaryText[6]}{binaryText[7]}{binaryText[8]}{binaryText[9]}", 2);
+
+                    if (min < 60)
+                    {
+                        result.Add($"{hour}:{min:00}");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = startIndex; i < 10; i++)
+                {
+                    binaryText[i] = 1;
+                    Read(num, i + 1, binaryText, result);
+                    binaryText[i] = 0;
+                }
+            }
+        }
+
+
+        [Test(Description = "https://leetcode.com/problems/binary-watch/")]
+        [Category("Easy")]
+        [Category("Leetcode")]
+        [Category("Binary Watch")]
+        [TestCaseSource("Input")]
+        public void Test1((int Output, int Input) item)
+        {
+            var response = ReadBinaryWatch(item.Input);
+            Assert.AreEqual(item.Output, response.Count);
+        }
+
+        public static IEnumerable<(int Output, int Input)> Input
+        {
+            get
+            {
+                return new List<(int Output, int Input)>()
+                {
+
+                    (10, 1),
+                };
+            }
+        }
+    }
+}
--- LeetCode/Medium/Letter Combinations of a Phone Number.cs
+++ LeetCode/Medium/Letter Combinations of a Phone Number.cs
@@ -1,9 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace LeetCode.Medium
 {
