4ff300eb6a0e3367328416e45140a6b406cd064b
Harshit Gindra <harshitgindra@gmail.com>
2020-09-16 18:39:45 -0500

    https://leetcode.com/problems/subsets/

---
+++ LeetCode/Medium/Subsets.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class SubsetsTest
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            IList<IList<int>> result = new List<IList<int>>()
+            {
+                new List<int>()
+            };
+            IList<int> combo = new List<int>();
+
+            for (int i = 1; i <= nums.Length; i++)
+            {
+                Sort(result, combo, nums, i, 0);
+            }
+
+            return result;
+        }
+
+        private void Sort(IList<IList<int>> result, IList<int> combo, int[] nums, int remaining, int index)
+        {
+            if (remaining == 0)
+            {
+                result.Add(combo.ToList());
+            }
+            else
+            {
+                for (int i = index; i <= nums.Length - remaining; i++)
+                {
+                    combo.Add(nums[i]);
+                    Sort(result, combo, nums, remaining - 1, i + 1);
+                    combo.Remove(nums[i]);
+                }
+            }
+        }
+
+        [Test(Description = "https://leetcode.com/problems/subsets/")]
+        [Category("Medium")]
+        [Category("Leetcode")]
+        [Category("Subsets")]
+        [TestCaseSource("Input")]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = Subsets(item.Input);
+            Assert.AreEqual(item.Output, response.Count);
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input
+        {
+            get
+            {
+                return new List<(int Output, int[] Input)>()
+                {
+
+                    (8, new int[] {1,2,3}),
+                };
+            }
+        }
+    }
+}
