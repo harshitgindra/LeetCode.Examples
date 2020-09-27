a0de910ec0c3eba1c95ccfd0b4160e45d210d972
Harshit Gindra <harshitgindra@gmail.com>
2020-09-27 15:17:19 -0500

    https://leetcode.com/problems/remove-element/

--- LeetCode/Easy/Remove Element.cs
+++ LeetCode/Easy/Remove Element.cs
@@ -9,40 +9,37 @@ namespace LeetCode.Easy
     {
         public int RemoveElement(int[] nums, int val)
         {
-            int returnValue = 0;
-
             if (nums != null && nums.Length > 0)
             {
-                Array.Sort(nums);
+                int returnValue = nums.Length;
 
-                int index = 0;
-                int nextIndex = 0;
-                while (index < nums.Length)
+                for (int i = 0; i < nums.Length; i++)
                 {
-                    if (nums[index] == val)
+                    if (nums[i] == val)
                     {
-                        nums[index] = nums[nextIndex];
-                        nextIndex++;
-                    }
-                    else
-                    {
-                        index++;
+                        nums[i] = int.MaxValue;
+                        returnValue--;
                     }
                 }
-            }
 
-            return returnValue;
+                Array.Sort(nums);
+                return returnValue;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
-        [Test(Description = "https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/")]
-        [Category("Medium")]
+        [Test(Description = "https://leetcode.com/problems/remove-element/")]
+        [Category("Easy")]
         [Category("Leetcode")]
-        [Category("Remove Duplicates from Sorted Array II")]
+        [Category("Remove Element")]
         [TestCaseSource("Input")]
         public void Test1((int Output, (int[], int) Input) item)
         {
             var response = RemoveElement(item.Input.Item1, item.Input.Item2);
-            //Assert.AreEqual(item.Output, response);
+            Assert.AreEqual(item.Output, response);
         }
 
         public static IEnumerable<(int Output, (int[], int) Input)> Input
