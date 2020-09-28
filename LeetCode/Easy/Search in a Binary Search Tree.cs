b835ed8398ab3ef223d4b812ec8bc40b554f7099
Harshit Gindra <harshitgindra@gmail.com>
2020-09-28 14:24:52 -0500

    BST search

---
+++ LeetCode/Easy/Search in a Binary Search Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Search_in_a_Binary_Search_Tree
+    {
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return default;
+            }
+
+            if (root.val == val)
+            {
+                return root;
+            }
+
+            var response = SearchBST(root.left, val);
+            if (response == null)
+            {
+                response = SearchBST(root.right, val);
+            }
+
+            return response;
+        }
+
+    }
+}
