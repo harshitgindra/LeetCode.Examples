f732fb447247661538e415d2cc910d9621ddb36c
Harshit Gindra <harshitgindra@gmail.com>
2020-09-28 00:33:47 -0500

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/

---
+++ LeetCode/Easy/Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Best_Time_to_Buy_and_Sell_Stock_II
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int sum = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                int diff = prices[i] - prices[i - 1];
+                if (diff > 0)
+                {
+                    sum += diff;
+                }
+            }
+
+            return sum;
+        }
+
+        public int MaxProfit2(int[] prices)
+        {
+            int result = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                result = Math.Max(result, Check(prices, prices[i], i + 1, 0, 0, true));
+            }
+            return result;
+        }
+
+        private int Check(int[] prices, int buyPrice, int day, int tempProfit, int maxProfit, bool isPurchased)
+        {
+            if (day >= prices.Length)
+            {
+                return Math.Max(tempProfit, maxProfit);
+            }
+            else
+            {
+                for (int i = day; i < prices.Length; i++)
+                {
+                    int dayPrice = prices[i];
+                    if (isPurchased)
+                    {
+                        if (buyPrice < dayPrice)
+                        {
+                            int p = dayPrice - buyPrice;
+                            tempProfit += p;
+
+                            maxProfit = Check(prices, dayPrice, i + 1, tempProfit, maxProfit, false);
+
+                            tempProfit -= p;
+                        }
+                    }
+                    else
+                    {
+                        maxProfit = Check(prices, dayPrice, i + 1, tempProfit, maxProfit, true);
+                    }
+                }
+                return maxProfit;
+            }
+        }
+
+        [Test(Description = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/")]
+        [Category("Easy")]
+        [Category("Leetcode")]
+        [Category("Best Time to Buy and Sell Stock II")]
+        [TestCaseSource("Input")]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = MaxProfit(item.Input);
+            Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input
+        {
+            get
+            {
+                return new List<(int Output, int[] Input)>()
+                {
+
+                    (8, (new int[] {3,3,5,0,0,3,1,4})),
+                    (7, (new int[] {7,1,5,3,6,4})),
+                    (4, (new int[] {1,2,3,4,5})),
+                    (0, (new int[] {7,6,4,3,1})),
+                };
+            }
+        }
+    }
+}
