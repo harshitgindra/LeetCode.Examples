0826a3450713a85a3c7d0ed7c50a3452445ce415
Harshit Gindra <harshitgindra@gmail.com>
2020-09-28 18:48:43 -0500

    https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/

---
+++ LeetCode/Easy/Maximum Depth of Binary Tree.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Maximum_Depth_of_Binary_Tree
+    {
+        //https://leetcode.com/problems/maximum-depth-of-binary-tree/submissions/
+        public int MaxDepth(TreeNode root)
+        {
+            int max = Check(root, 0, 0);
+            return max;
+        }
+
+
+        private int Check(TreeNode node, int level, int maxLevel)
+        {
+            if (node == null)
+            {
+                return Math.Max(level, maxLevel);
+            }
+            else
+            {
+                maxLevel = Check(node.left, level + 1, maxLevel);
+
+                maxLevel = Check(node.right, level + 1, maxLevel);
+
+                return maxLevel;
+            }
+        }
+    }
+}
