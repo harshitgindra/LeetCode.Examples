d9b54a0f22f5ffd06018e11deae99ecae512c0f8
Harshit Gindra <harshitgindra@gmail.com>
2020-10-03 11:11:44 -0500

    https://leetcode.com/problems/binary-tree-level-order-traversal/

---
+++ LeetCode/Medium/Binary Tree Level Order Traversal.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class Binary_Tree_Level_Order_Traversal
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            SortedDictionary<int, IList<int>> dict = new SortedDictionary<int, IList<int>>();
+            Process(root, 0, dict);
+            return dict.Select(x => x.Value).ToList();
+        }
+
+        private void Process(TreeNode node, int level, SortedDictionary<int, IList<int>> dict)
+        {
+            if (node != null)
+            {
+                Process(node.left, level + 1, dict);
+
+                Process(node.right, level + 1, dict);
+
+                if (dict.ContainsKey(level))
+                {
+                    dict[level].Add(node.val);
+                }
+                else
+                {
+                    dict.Add(level, new List<int>() { node.val });
+                }
+            }
+        }
+
+        [Test(Description = "https://leetcode.com/problems/binary-tree-level-order-traversal/")]
+        [Category("Medium")]
+        [Category("Leetcode")]
+        [Category("Binary Tree Level Order Traversal")]
+        [TestCaseSource("Input")]
+        public void Test1((IList<IList<int>> Output, TreeNode Input) item)
+        {
+            var response = this.LevelOrder(item.Input);
+            Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(IList<IList<int>> Output, TreeNode Input)> Input
+        {
+            get
+            {
+                return new List<(IList<IList<int>> Output, TreeNode Input)>()
+                {
+                    (new List<IList<int>>(){
+                    new List<int>(){ 3},
+                    new List<int>(){ 9,20},
+                    new List<int>(){ 15,7},
+                        }, new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))))
+                };
+            }
+        }
+    }
+}
