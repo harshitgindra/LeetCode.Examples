23dfcb716b5965f23f4dba23e1857da58b7cb229
Harshit Gindra <harshitgindra@gmail.com>
2020-10-03 11:41:36 -0500

    https://leetcode.com/problems/min-stack/

---
+++ LeetCode/Easy/MinStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    public class MinStack
+    {
+        /** initialize your data structure here. */
+        readonly List<int> _data;
+        int _lastIndex = 0;
+        public MinStack()
+        {
+            _data = new List<int>();
+            _lastIndex = -1;
+        }
+
+        public void Push(int x)
+        {
+            _data.Add(x);
+            _lastIndex++;
+        }
+
+        public void Pop()
+        {
+
+            _data.RemoveAt(_lastIndex);
+            _lastIndex--;
+        }
+
+        public int Top()
+        {
+            return _data[_lastIndex];
+        }
+
+        public int GetMin()
+        {
+            return _data.Min();
+        }
+    }
+
+
+}
---
+++ LeetCode/Medium/Shuffle an Array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class Shuffle_an_Array
+    {
+        public class Solution
+        {
+            private int[] _data;
+            public Solution(int[] nums)
+            {
+                _data = nums;
+            }
+
+            /** Resets the array to its original configuration and return it. */
+            public int[] Reset()
+            {
+                return _data;
+            }
+
+            /** Returns a random shuffling of the array. */
+            public int[] Shuffle()
+            {
+                return _data.OrderBy(x => Guid.NewGuid()).ToArray();
+            }
+        }
+    }
+}
