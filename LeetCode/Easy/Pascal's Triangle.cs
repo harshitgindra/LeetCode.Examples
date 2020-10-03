e20be45029b3218c3bb90dd7dcd76eb9bff07db8
Harshit Gindra <harshitgindra@gmail.com>
2020-10-03 12:38:19 -0500

    https://leetcode.com/problems/pascals-triangle/

---
+++ LeetCode/Easy/Pascal's Triangle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Pascal_s_Triangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> results = new List<IList<int>>()
+            {
+            };
+
+            if (numRows != 0)
+            {
+                var prev = new List<int>() { 1 };
+                results.Add(prev);
+
+                if (numRows > 1)
+                {
+                    while (true)
+                    {
+                        prev = Start(prev);
+                        results.Add(prev);
+                        if (prev.Count == numRows)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+            }
+
+            return results;
+        }
+
+        private List<int> Start(IList<int> nums)
+        {
+            int numLength = nums.Count;
+            var tempList = nums.ToList();
+            tempList.Add(nums[0]);
+
+            for (int i = 0; i < numLength - 1; i++)
+            {
+                tempList[i + 1] = nums[i] + nums[i + 1];
+            }
+
+            return tempList;
+        }
+    }
+}
