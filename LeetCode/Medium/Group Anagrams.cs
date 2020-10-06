e7d541cdca283cef30329f5b7ec3a2a0076cbaaa
Harshit Gindra <harshitgindra@gmail.com>
2020-10-06 19:14:43 -0400

    group analgrams

---
+++ LeetCode/Medium/Group Anagrams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class Group_Anagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            IDictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                var o = string.Join("", strs[i].OrderBy(x => x));
+                if (dict.ContainsKey(o))
+                {
+                    dict[o].Add(strs[i]);
+                }
+                else
+                {
+                    dict.Add(o, new List<string>() { strs[i] });
+                }
+            }
+
+            IList<IList<string>> ret = new List<IList<string>>();
+            foreach (var item in dict)
+            {
+                ret.Add(item.Value.ToList());
+            }
+
+            return ret;
+        }
+    }
+}
