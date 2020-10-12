973ddd54e9d0a4b093d09e66475d575386d6cbe7
Harshit Gindra <harshitgindra@gmail.com>
2020-10-11 23:01:42 -0500

    https://leetcode.com/problems/two-sum-iv-input-is-a-bst/

---
+++ LeetCode/Easy/Two Sum IV - Input is a BST.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    //https://leetcode.com/problems/two-sum-iv-input-is-a-bst/submissions/
+    class Two_Sum_IV___Input_is_a_BST
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            List<int> result = new List<int>();
+            Read(result, root);
+
+            for (int i = 0; i < result.Count - 1; i++)
+            {
+                var diff = k - result[i];
+                for (int j = i + 1; j < result.Count; j++)
+                {
+                    if (result[j] == diff)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void Read(List<int> nums, TreeNode node)
+        {
+            if (node != null)
+            {
+                nums.Add(node.val);
+                Read(nums, node.left);
+                Read(nums, node.right);
+            }
+        }
+    }
+}
