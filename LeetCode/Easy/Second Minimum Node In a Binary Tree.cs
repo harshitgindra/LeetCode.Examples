c73789f61902040b982372d0a08df78eeac8f1d9
Harshit Gindra <harshitgindra@gmail.com>
2020-10-12 19:37:35 -0500

    Second_Minimum_Node_In_a_Binary_Tree

---
+++ LeetCode/Easy/Second Minimum Node In a Binary Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Second_Minimum_Node_In_a_Binary_Tree
+    {
+        public int FindSecondMinimumValue(TreeNode root)
+        {
+            HashSet<int> result = new HashSet<int>();
+            Read(root, result);
+
+            if (result.Count < 2)
+            {
+                return -1;
+            }
+            else
+            {
+                return result.OrderBy(x => x).ElementAt(1);
+            }
+        }
+
+        private void Read(TreeNode node, HashSet<int> result)
+        {
+            if (node != null)
+            {
+                result.Add(node.val);
+                Read(node.left, result);
+                Read(node.right, result);
+            }
+        }
+    }
+}
