e0b1ccd4deb8ac2e523ce4e144edf16ac1687526
Harshit Gindra <harshitgindra@gmail.com>
2020-10-14 00:59:55 -0500

    https://leetcode.com/problems/backspace-string-compare/

---
+++ LeetCode/Easy/Backspace String Compare.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/backspace-string-compare/
+    /// </summary>
+    class Backspace_String_Compare
+    {
+        public bool BackspaceCompare(string S, string T)
+        {
+            S = Parse(S);
+            T = Parse(T);
+
+            return S == T;
+        }
+
+        private string Parse(string str)
+        {
+            List<char> ret = new List<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '#')
+                {
+                    if (ret.Count > 1)
+                    {
+                        ret.RemoveAt(ret.Count - 1);
+                    }
+                }
+                else
+                {
+                    ret.Add(str[i]);
+                }
+            }
+
+            return string.Join("", ret);
+        }
+    }
+}
