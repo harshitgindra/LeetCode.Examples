ae9709cc1e51e3030ec74e9f2163902a4860c8ac
Harshit Gindra <harshitgindra@gmail.com>
2020-10-15 14:37:14 -0400

    Todo

---
+++ LeetCode/Easy/Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    class Binary_Tree_Level_Order_Traversal_II
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            IDictionary<int, IList<int>> result = new Dictionary<int, IList<int>>();
+            Read(root, 0, result);
+
+            for (int i = 1; i < result.Count; i = i + 2)
+            {
+                result[i].Reverse();
+            }
+
+            return result.Values.ToList();
+        }
+
+        private void Read(TreeNode node, int level, IDictionary<int, IList<int>> result)
+        {
+            if (node != null)
+            {
+                Read(node.left, level + 1, result);
+                Read(node.right, level + 1, result);
+
+                if (result.ContainsKey(level))
+                {
+                    result[level].Add(node.val);
+                }
+                else
+                {
+                    result.Add(level, new List<int>() { node.val });
+                }
+            }
+        }
+    }
+}
