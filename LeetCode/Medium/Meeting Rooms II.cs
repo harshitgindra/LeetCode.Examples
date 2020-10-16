006d4713359325e7785100e5ad3039736a0e371e
Harshit Gindra <harshitgindra@gmail.com>
2020-10-15 22:29:03 -0500

    https://leetcode.com/problems/meeting-scheduler/

---
+++ LeetCode/Medium/Meeting Rooms II.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class Meeting_Rooms_II
+    {
+        public int MinMeetingRooms(int[][] intervals)
+        {
+            int[] start = new int[intervals.Length];
+            int[] end = new int[intervals.Length];
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                start[i] = intervals[i][0];
+                end[i] = intervals[i][1];
+            }
+
+            Array.Sort(start);
+            Array.Sort(end);
+
+            int ret = 0;
+            for (int i = 0, j = 0; i < start.Length; i++)
+            {
+                if (start[i] < end[j])
+                {
+                    ret++;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            return ret;
+        }
+    }
+}
