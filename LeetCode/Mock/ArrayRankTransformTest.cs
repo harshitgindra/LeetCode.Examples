6004193c176937e47da475436d9243aa0be3a3a4
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 12:50:08 -0500

    https://leetcode.com/problems/rank-transform-of-an-array/

---
+++ LeetCode/Mock/ArrayRankTransformTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Mock
+{
+    /// <summary>
+    /// https://leetcode.com/problems/rank-transform-of-an-array/
+    /// </summary>
+    class ArrayRankTransformTest
+    {
+        public int[] ArrayRankTransform(int[] arr)
+        {
+            int index = 1;
+            var unique = arr.Distinct().OrderBy(x => x).ToDictionary(x => x, y => index++);
+
+            int[] ret = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                ret[i] = unique[arr[i]];
+            }
+            return ret;
+        }
+    }
+}
