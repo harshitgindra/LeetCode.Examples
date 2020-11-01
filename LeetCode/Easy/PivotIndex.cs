5e47295d8575705da7541735255656a2eb8e2790
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 14:03:08 -0500

    https://leetcode.com/problems/find-pivot-index/

---
+++ LeetCode/Easy/PivotIndex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-pivot-index/
+    /// </summary>
+    class PivotIndexTest
+    {
+        public int PivotIndex(int[] nums)
+        {
+            int rhs = nums.Sum();
+
+            int lhs = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rhs = rhs - nums[i];
+                if (lhs == rhs)
+                {
+                    return i;
+                }
+                else
+                {
+                    lhs += nums[i];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
