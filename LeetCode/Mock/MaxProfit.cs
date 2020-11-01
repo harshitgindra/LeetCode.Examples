f8e4e59ac90a5cfe3b59d932ba448f4d77aabdea
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 13:19:23 -0500

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ LeetCode/Mock/MaxProfit.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Mock
+{
+    /// <summary>
+    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    /// </summary>
+    class MaxProfitTest
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                maxProfit = Math.Max(maxProfit, Trade(prices, i + 1, prices[i], maxProfit));
+            }
+            return maxProfit;
+        }
+
+        private int Trade(int[] prices, int day, int purchasedAt, int maxProfit)
+        {
+            for (int i = day; i < prices.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[i] - purchasedAt);
+            }
+            return maxProfit;
+        }
+    }
+}
---
+++ LeetCode/Mock/ProductExceptSelf.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Mock
+{
+    /// <summary>
+    /// https://leetcode.com/problems/product-of-array-except-self/
+    /// </summary>
+    class ProductExceptSelfTest
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] returnValue = new int[nums.Length];
+
+            int product = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                product *= nums[i];
+                returnValue[i] = product;
+            }
+
+            product = 1;
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                returnValue[i] = product * (i == 0 ? 1 : returnValue[i - 1]);
+                product *= nums[i];
+            }
+
+            return returnValue;
+        }
+    }
+}
