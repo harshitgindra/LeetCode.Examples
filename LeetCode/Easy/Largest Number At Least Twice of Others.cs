cdc333f7f3bf838d9130e4f2009065d800278a76
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 14:17:40 -0500

    Largest_Number_At_Least_Twice_of_Others

---
+++ LeetCode/Easy/Largest Number At Least Twice of Others.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Largest_Number_At_Least_Twice_of_Others
+    {
+
+        public int DominantIndex(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+            else if (nums.Length > 1)
+            {
+                int[] ret = nums.ToArray();
+
+                Array.Sort(ret);
+
+                int max = ret[nums.Length - 1];
+
+                if (max >= ret[nums.Length - 2] * 2)
+                {
+                    return Array.IndexOf(nums, max);
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
