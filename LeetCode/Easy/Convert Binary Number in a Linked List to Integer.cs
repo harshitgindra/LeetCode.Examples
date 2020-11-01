92362d1250b5225b5729d2c5cd1ac91101959cbb
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 12:39:44 -0500

    https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/submissions/

---
+++ LeetCode/Easy/Convert Binary Number in a Linked List to Integer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/convert-binary-number-in-a-linked-list-to-integer/submissions/
+    /// </summary>
+    class Convert_Binary_Number_in_a_Linked_List_to_Integer
+    {
+        public int GetDecimalValue(ListNode head)
+        {
+            string key = Read(head, "");
+
+            return Convert.ToInt32(key, 2);
+        }
+
+        private string Read(ListNode node, string key)
+        {
+            if (node != null)
+            {
+                return Read(node.next, $"{key}{node.val}");
+            }
+            return key;
+        }
+    }
+}
