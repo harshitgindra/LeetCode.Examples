9e9f75265ced9e37c825c51c63a849d3da2c927e
Harshit Gindra <harshitgindra@gmail.com>
2020-11-01 19:50:47 -0500

    https://leetcode.com/problems/array-partition-i/

---
+++ LeetCode/Easy/Array Partition I.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Easy
+{
+    class Array_Partition_I
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/array-partition-i/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public int ArrayPairSum(int[] nums)
+        {
+            Array.Sort(nums);
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i = i + 2)
+            {
+                sum += nums[i];
+            }
+
+            return sum;
+        }
+    }
+}
