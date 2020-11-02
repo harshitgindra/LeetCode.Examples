686b92bc086895f15591100a1f3c8ae0d952e5e5
Harshit Gindra <harshitgindra@gmail.com>
2020-11-02 18:51:52 -0500

    https://leetcode.com/problems/rotate-list/

---
+++ LeetCode/Medium/Rotate List.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class Rotate_List
+    {
+        public ListNode RotateRight(ListNode head, int k)
+        {
+
+            if (head == null || head.next == null) return head;
+            ListNode tail = head, current = head;
+            int length = 1;
+            while (tail.next != null)
+            {
+                length++;
+                tail = tail.next;
+            }
+            k = k % length;
+            if (k == 0) return head;
+            for (int i = 0; i < length - k - 1; i++)
+            {
+                current = current.next;
+            }
+
+            var newHead = current.next;
+            tail.next = head;
+            current.next = null;
+            return newHead;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/rotate-list/")]
+        [Category("Easy")]
+        [Category("Leetcode")]
+        [Category("Rotate List")]
+        [TestCaseSource("Input")]
+        public void Test1((ListNode Output, (ListNode, int) Input) item)
+        {
+            var response = RotateRight(item.Input.Item1, item.Input.Item2);
+           // Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(ListNode Output, (ListNode, int) Input)> Input
+        {
+            get
+            {
+                return new List<(ListNode Output, (ListNode, int) Input)>()
+                {
+                    (new ListNode(3, new ListNode(4, new ListNode(1, new ListNode(2)))),
+
+                    (new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))),2)
+                    )
+                };
+            }
+        }
+    }
+}
