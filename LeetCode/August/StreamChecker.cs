e5e4e34b737d9d390a8a4829309e690fab7e6ee1
Harshit Gindra <harshitgindra@gmail.com>
2020-11-07 21:36:16 -0600

    nuget update and solving a contest

---
+++ LeetCode/August/StreamChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.August
+{
+    public class StreamChecker
+    {
+        private HashSet<string> _words;
+        private string _lastQuery;
+
+        public StreamChecker(string[] words)
+        {
+            _words = words.ToHashSet();
+            _lastQuery = default;
+        }
+
+        public bool Query(char letter)
+        {
+            _lastQuery += letter;
+            if (_words.Contains($"{letter}"))
+            {
+                return true;
+            }
+            else
+            {
+                for (int i = _lastQuery.Length - 2; i > 0; i--)
+                {
+                    var newWord = _lastQuery.Substring(i);
+                    if (_words.Any(x=>x.EndsWith(newWord)))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
