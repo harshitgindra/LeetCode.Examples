6471122de528faae350d8cfa2a78ca5ae805ae72
Harshit Gindra <harshitgindra@gmail.com>
2020-11-18 10:46:40 -0500

    https://leetcode.com/problems/top-k-frequent-elements/

---
+++ LeetCode/Medium/Top K Frequent Elements.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/top-k-frequent-elements/
+    /// </summary>
+    class Top_K_Frequent_Elements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+
+            foreach (var item in nums)
+            {
+                if (dict.ContainsKey(item))
+                {
+                    dict[item]++;
+                }
+                else
+                {
+                    dict.Add(item, 1);
+                }
+            }
+
+            return dict.OrderByDescending(x => x.Value)
+                .Take(k)
+                .Select(x => x.Key)
+                .ToArray();
+        }
+    }
+}
