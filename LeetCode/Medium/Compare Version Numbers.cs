433b654330c8212f206137040e9956fbb2365958
Harshit Gindra <harshitgindra@gmail.com>
2020-11-23 18:23:28 -0600

    https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/

---
+++ LeetCode/Medium/Compare Version Numbers.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace LeetCode.Medium
+{
+    public class Compare_Version_Numbers
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            var v1 = version1.Split(".")
+                .Select(x => Int32.Parse(x))
+                .ToList();
+
+            var v2 = version2.Split(".")
+                .Select(x => Int32.Parse(x))
+                .ToList();
+
+            var maxLength = Math.Max(v1.Count, v2.Count);
+
+            while (v1.Count < maxLength)
+            {
+                v1.Add(0);
+            }
+
+            while (v2.Count < maxLength)
+            {
+                v2.Add(0);
+            }
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (v1[i] > v2[i])
+                {
+                    return 1;
+                }
+                else if (v1[i] < v2[i])
+                {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/combination-sum/")]
+        [Category("Medium")]
+        [Category("Leetcode")]
+        [Category("Combination Sum")]
+        [TestCaseSource("Input")]
+        public void Test1((int Output, (string, string) Input) item)
+        {
+            var response = CompareVersion(item.Input.Item1, item.Input.Item2);
+            Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(int Output, (string, string) Input)> Input
+        {
+            get
+            {
+                return new List<(int Output, (string, string) Input)>()
+                {
+                    (0, ("1.01", "1.001")),
+                };
+            }
+        }
+    }
+}
---
+++ LeetCode/Medium/Find Nearest Right Node in Binary Tree.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
+    /// </summary>
+    public class Find_Nearest_Right_Node_in_Binary_Tree
+    {
+        public TreeNode FindNearestRightNode(TreeNode root, TreeNode u)
+        {
+            var result = Read(root, u, 1, new Dictionary<int, List<TreeNode>>());
+
+            TreeNode returnValue = default;
+
+            foreach (var item in result)
+            {
+                if (item.Value.Any(x => x.val == u.val))
+                {
+                    for (int i = 1; i < item.Value.Count; i++)
+                    {
+                        if (item.Value[i - 1].val == u.val)
+                        {
+                            returnValue = item.Value[i];
+                        }
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+
+        private Dictionary<int, List<TreeNode>> Read(TreeNode node, TreeNode u, int level,
+            Dictionary<int, List<TreeNode>> result)
+        {
+            if (node != null)
+            {
+                if (result.ContainsKey(level))
+                {
+                    result[level].Add(node);
+                }
+                else
+                {
+                    result.Add(level, new List<TreeNode>() {node});
+                }
+
+                result = Read(node.left, u, level + 1, result);
+                result = Read(node.right, u, level + 1, result);
+            }
+
+            return result;
+        }
+    }
+}
