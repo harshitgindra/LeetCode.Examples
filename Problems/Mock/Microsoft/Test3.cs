33396b81a4eba673ab0bf82931f35ad894529e02
Harshit Gindra <harshitgindra@gmail.com>
2021-02-23 22:30:03 -0500

    Renaming project names and properties

--- Problems/Mock/Microsoft/Test3.cs
+++ Problems/Mock/Microsoft/Test3.cs
@@ -2,8 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using System.Text;
 
 namespace LeetCode.Mock.Microsoft
 {
