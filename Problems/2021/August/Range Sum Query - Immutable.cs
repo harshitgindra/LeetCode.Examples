2cdfa76b73f7514dbc64cdb290f977f76ad55f26
Harshit Gindra <harshitgindra@gmail.com>
2021-08-17 10:08:29 -0500

    https://leetcode.com/problems/range-sum-query-immutable/

---
+++ Problems/2021/August/Range Sum Query - Immutable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems._2021.August
+{
+    /// <summary>
+    /// https://leetcode.com/problems/range-sum-query-immutable/solution/
+    /// </summary>
+    class NumArray
+    {
+        private readonly IDictionary<int, int> _nums;
+
+        public NumArray(int[] nums)
+        {
+            _nums = new Dictionary<int, int>();
+            int i = 0;
+            foreach (var item in nums)
+            {
+                _nums.Add(i++, item);
+            }
+        }
+
+        public int SumRange(int left, int right)
+        {
+            int total = 0;
+            for(int i = left; i<=right; i++)
+            {
+                if (_nums.ContainsKey(i))
+                {
+                    total+= _nums[i];
+                }
+            }
+
+            return total;
+        }
+    }
+}
