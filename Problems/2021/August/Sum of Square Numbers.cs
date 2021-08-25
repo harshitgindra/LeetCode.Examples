40951a62812623d4197169b9a6743b4cf7509819
Harshit Gindra <harshitgindra@gmail.com>
2021-08-25 11:11:25 -0500

    https://leetcode.com/problems/sum-of-square-numbers/

---
+++ Problems/2021/August/Sum of Square Numbers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems._2021.August
+{
+    /// <summary>
+    /// https://leetcode.com/problems/sum-of-square-numbers/
+    /// </summary>
+    class Sum_of_Square_Numbers
+    {
+        public bool JudgeSquareSum(int c)
+        {
+            var upper = (int)Math.Sqrt(c);
+            var lower = 0;
+
+            while (lower <= upper)
+            {
+                //***
+                //*** Applying the formula a*a + b*b = c
+                //***
+                var total = lower * lower + upper * upper;
+                if (total == c)
+                {
+                    return true;
+                }
+                else if (total > c)
+                {
+                    //***
+                    //*** Total greater than c
+                    //*** decreement the upper value
+                    //***
+                    upper--;
+                }
+                else
+                {
+                    //***
+                    //*** Total less than c
+                    //*** increement the upper value
+                    //***
+                    lower++;
+                }
+            }
+            return false;
+        }
+    }
+}
