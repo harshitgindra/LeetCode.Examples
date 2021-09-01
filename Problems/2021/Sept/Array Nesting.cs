6c65ebe4e90736d015fe28b642099163a2a1d870
Harshit Gindra <harshitgindra@gmail.com>
2021-09-01 08:39:32 -0500

    https://leetcode.com/problems/array-nesting/

---
+++ Problems/2021/Sept/Array Nesting.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems._2021.Sept
+{
+    /// <summary>
+    /// https://leetcode.com/problems/array-nesting/
+    /// </summary>
+    class Array_Nesting
+    {
+        public int ArrayNesting(int[] nums)
+        {
+            //***
+            //*** Initialize the array with all false
+            //***
+            bool[] isVisited = new bool[nums.Length];
+            int returnValue = 0;
+            //***
+            //*** Iterate through all the elements in the array
+            //***
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //***
+                //*** If the element is already visited
+                //*** We do not have to continue with the index as the result will be less than the previous calculation
+                //***
+                if (!isVisited[i])
+                {
+                    int start = nums[i];
+                    int count = 0;
+                    //***
+                    //*** Run the loop until we find the element matching nums[i]
+                    //***
+                    do
+                    {
+                        start = nums[start];
+                        count++;
+                        isVisited[start] = true;
+                    }
+                    while (start != nums[i]);
+                    //***
+                    //*** Compare the count with returnValue and save the higher value
+                    //***
+                    returnValue = Math.Max(returnValue, count);
+                }
+            }
+            return returnValue;
+        }
+    }
+}
