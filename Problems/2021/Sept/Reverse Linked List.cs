ec9d61736e9ad6ee5bfd595af4a411e8431315de
Harshit Gindra <harshitgindra@gmail.com>
2021-09-07 10:44:41 -0500

    https://leetcode.com/problems/reverse-linked-list/

---
+++ Problems/2021/Sept/Reverse Linked List.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems._2021.Sept
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-linked-list/
+    /// </summary>
+    class Reverse_Linked_List
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode prev = null;
+            ListNode cur = head;
+
+            while (cur != null)
+            {
+                ListNode post = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = post;
+            }
+
+            return prev;
+        }
+    }
+}
