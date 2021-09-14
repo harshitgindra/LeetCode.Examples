b9252d30ab65547512230d1e533baf01db003960
Harshit Gindra <harshitgindra@gmail.com>
2021-09-14 11:29:35 -0500

    https://leetcode.com/problems/reverse-only-letters/

---
+++ Problems/2021/Sept/Reverse Only Letters.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Problems._2021.Sept
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-only-letters/
+    /// </summary>
+    class Reverse_Only_Letters
+    {
+        public string ReverseOnlyLetters(string S)
+        {
+            if (string.IsNullOrEmpty(S))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                var arry = S.ToCharArray();
+                int sIndex = 0;
+                int eIndex = S.Length - 1;
+                while (sIndex < eIndex)
+                {
+                    if (char.IsLetter(arry[sIndex]))
+                    {
+                        if (char.IsLetter(arry[eIndex]))
+                        {
+                            var temp = arry[eIndex];
+                            arry[eIndex--] = arry[sIndex];
+                            arry[sIndex++] = temp;
+                        }
+                        else
+                        {
+                            eIndex--;
+                        }
+                    }
+                    else
+                    {
+                        sIndex++;
+                    }
+                }
+
+                return string.Join("", arry);
+            }
+        }
+    }
+}
