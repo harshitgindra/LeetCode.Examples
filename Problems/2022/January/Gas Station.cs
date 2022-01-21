80d0d78665104e0d8a54864bc9e19b3334940686
Harshit Gindra <harshitgindra@gmail.com>
2022-01-20 23:24:20 -0800

    https://leetcode.com/problems/gas-station/submissions/

---
+++ Problems/2022/January/Gas Station.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+
+namespace Leetcode.Problems._2022.January
+{
+    /// <summary>
+    /// https://leetcode.com/problems/gas-station/submissions/
+    /// </summary>
+    public class Gas_Station
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            if (gas.Sum() >= cost.Sum())
+            {
+                int returnValue = 0;
+                int total = 0;
+                for (int i = 0; i < gas.Length; i++)
+                {
+                    total += gas[i] - cost[i];
+                    if (total < 0)
+                    {
+                        total = 0;
+                        returnValue = i + 1;
+                    }
+                }
+
+                return returnValue;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
