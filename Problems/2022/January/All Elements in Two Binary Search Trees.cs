2372e6cbf9464e1c1e73ed6ca1f8ce4f4a3d1e4c
Harshit Gindra <harshitgindra@gmail.com>
2022-01-26 00:07:17 -0800

    https://leetcode.com/problems/all-elements-in-two-binary-search-trees/

---
+++ Problems/2022/January/All Elements in Two Binary Search Trees.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using LeetCode;
+
+namespace Leetcode.Problems._2022.January
+{
+    /// <summary>
+    /// https://leetcode.com/problems/all-elements-in-two-binary-search-trees/
+    /// </summary>
+    public class All_Elements_in_Two_Binary_Search_Trees
+    {
+        private IList<int> _nums;
+        public IList<int> GetAllElements(TreeNode root1, TreeNode root2)
+        {
+            _nums = new List<int>();
+            _Helper(root1);
+            _Helper(root2);
+
+            return _nums
+                .OrderBy(x=>x)
+                .ToList();
+        }
+
+        private void _Helper(TreeNode node)
+        {
+            if (node != null)
+            {
+                _nums.Add(node.val);
+                _Helper(node.left);
+                _Helper(node.right);
+            }
+        }
+    }
+}
