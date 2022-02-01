e43eb34fa444017951437dd192330d78a555c7f5
Harshit Gindra <harshitgindra@gmail.com>
2022-01-31 17:40:53 -0800

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/

---
+++ Problems/Easy/Best Time to Buy and Sell Stock.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Leetcode.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+    /// </summary>
+    public class Best_Time_to_Buy_and_Sell_Stock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profit = 0;
+
+            if (prices != null && prices.Length > 1)
+            {
+                int bestPriceToBuy = prices[0];
+
+                for (int i = 1; i < prices.Length; i++)
+                {
+                    profit = Math.Max(profit, prices[i] - bestPriceToBuy);
+                    bestPriceToBuy = Math.Min(bestPriceToBuy, prices[i]);
+                }
+            }
+
+            return profit;
+        }
+    }
+}
