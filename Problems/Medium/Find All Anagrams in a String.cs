d6113635ff2d8e793fc9e8c93c3e1a036c7b8cc3
Harshit Gindra <harshitgindra@gmail.com>
2022-02-01 17:45:10 -0800

    https://leetcode.com/problems/find-all-anagrams-in-a-string/

--- Problems/Medium/Find All Anagrams in a String.cs
+++ Problems/Medium/Find All Anagrams in a String.cs
@@ -5,158 +5,122 @@ using System.Linq;
 
 namespace LeetCode.Medium
 {
+    /// <summary>
+    /// https://leetcode.com/problems/find-all-anagrams-in-a-string/
+    /// </summary>
     class Find_All_Anagrams_in_a_String
     {
         public IList<int> FindAnagrams(string s, string p)
         {
-            IList<int> result = new List<int>();
-            int pLength = p.Length;
+            IList<int> returnValue = new List<int>();
 
-            if (pLength < s.Length)
+            if (!string.IsNullOrEmpty(s) && !string.IsNullOrEmpty(p)
+                                         && s.Length >= p.Length)
             {
-                var a = p.OrderBy(x => x).ToArray();
-                //Array.Sort(a);
-
-                string str = "";
-                bool prevAdded = false;
-                char prevChar = default;
-                for (int i = 0; i < s.Length; i++)
+                var pDictionary = _ParseStringToMap(p);
+                var sDictionary = _ParseStringToMap(s.Substring(0, p.Length));
+                //***
+                //*** Compare p and s dictionary
+                //***
+                if (_Compare(pDictionary, sDictionary))
                 {
-                    if (!p.Contains(s[i]))
+                    returnValue.Add(0);
+                }
+
+                int firstPointer = 0;
+
+                for (int i = p.Length; i < s.Length; i++)
+                {
+                    char prevChar = s[firstPointer];
+                    char nextChar = s[i];
+                    //***
+                    //*** Remove the char pointed by first pointer from the dictionary
+                    //***
+                    if (sDictionary[prevChar] == 1)
                     {
-                        str = "";
-                        prevAdded = false;
+                        sDictionary.Remove(prevChar);
                     }
                     else
                     {
-                        str += s[i];
-                        if (prevAdded && prevChar == s[i])
-                        {
-                            result.Add(i - pLength + 1);
-                            prevChar = str[0];
-                            str = str.Substring(1);
-                        }
-                        else if (str.Length == pLength)
-                        {
-                            if (IsAnagrams(a, str))
-                            {
-                                result.Add(i - pLength + 1);
-                                prevAdded = true;
-                            }
-                            prevChar = str[0];
-                            str = str.Substring(1);
-                        }
-                        else if (str.Length > p.Length)
-                        {
-                            str = str.Substring(1);
-                        }
-                        else
-                        {
-                            prevAdded = false;
-                        }
+                        sDictionary[prevChar]--;
                     }
-                }
-            }
-
-            return result;
-        }
-
-        private bool IsAnagrams(char[] a, string str)
-        {
-            int index = 0;
-            foreach (var item in str.OrderBy(x => x))
-            {
-                if (a[index] != item)
-                {
-                    return false;
-                }
-                index++;
-            }
-            return true;
-        }
-
-        public IList<int> FindAnagrams1(string s, string p)
-        {
-            var pOrdered = p.OrderBy(x => x);
-            IList<int> result = new List<int>();
-
-            if (p.Length < s.Length)
-            {
-                int index = 0;
-                while (index < s.Length)
-                {
-                    string sb = "";
-                    for (int i = index; i < index + p.Length; i++)
+                    //***
+                    //*** Add the new char to the dictionary
+                    //***
+                    if (sDictionary.ContainsKey(nextChar))
                     {
-                        if (!p.Contains(s[i]))
-                        {
-                            index = i + 1;
-                            break;
-                        }
-                        else
-                        {
-                            sb += s[i];
-                        }
+                        sDictionary[nextChar]++;
                     }
-
-                    if (sb.Length == p.Length)
+                    else
                     {
-                        var subStr = sb.OrderBy(x => x);
-                        if (IsAnagrams2(subStr, pOrdered, p.Length))
-                        {
-                            result.Add(index);
-                        }
-                        index++;
+                        sDictionary.Add(nextChar, 1);
                     }
-                }
+                    //***
+                    //*** Compare p and s dictionary
+                    //***
+                    if (_Compare(pDictionary, sDictionary))
+                    {
+                        returnValue.Add(i-p.Length + 1);
+                    }
 
+                    firstPointer++;
+                }
             }
 
-            return result;
+            return returnValue;
         }
 
-        private bool IsAnagrams2(IOrderedEnumerable<char> a, IOrderedEnumerable<char> b, int length)
+        private bool _Compare(IDictionary<char, int> dict1, IDictionary<char, int> dict2)
         {
-            for (int i = 0; i < length; i++)
+            foreach (var item in dict1)
             {
-                if (a.ElementAt(i) != b.ElementAt(i))
+                //***
+                //*** Check if key exists and value/count should be equal
+                //***
+                if (!dict2.ContainsKey(item.Key) || dict2[item.Key] != item.Value)
                 {
                     return false;
                 }
             }
+
             return true;
         }
+
+        private IDictionary<char, int> _ParseStringToMap(string str)
+        {
+            IDictionary<char, int> dict = new Dictionary<char, int>();
+            foreach (var c in str)
+            {
+                dict.TryAdd(c, 0);
+                dict[c]++;
+            }
+
+            return dict;
+        }
+
 
         public IList<int> FindAnagrams2(string s, string p)
         {
-            var a = p.ToCharArray();
-            Array.Sort(a);
+            IList<int> returnValue = new List<int>();
 
-            IList<int> result = new List<int>();
-
-            List<char> b = new List<char>
+            if (!string.IsNullOrEmpty(s) && !string.IsNullOrEmpty(p)
+                                         && s.Length >= p.Length)
             {
-                '-'
-            };
-            for (int i = 0; i < p.Length - 1; i++)
-            {
-                b.Add(s[i]);
-            }
-
-            for (int i = p.Length - 1; i <= s.Length - 1; i++)
-            {
-                b.RemoveAt(0);
-                b.Add(s[i]);
-
-                //if (IsAnagrams(a, b))
+                p = string.Join("", p.OrderBy(x => x));
+                int pLength = p.Length;
+                for (int i = 0; i <= s.Length - p.Length; i++)
                 {
-                    result.Add(i + 1 - p.Length);
+                    string temp = s.Substring(i, pLength);
+                    temp = string.Join("", temp.OrderBy(x => x));
+                    if (p == temp)
+                    {
+                        returnValue.Add(i);
+                    }
                 }
             }
 
-            return result;
+            return returnValue;
         }
-
 
 
         [Test(Description = "https://leetcode.com/problems/valid-anagram/")]
@@ -176,7 +140,6 @@ namespace LeetCode.Medium
             {
                 return new List<(int Output, (string, string) Input)>()
                 {
-
                     (2, ("cbaebabacd", "abc")),
                     (3, ("abab", "ab")),
                     (4, ("abacbabc", "abc")),
