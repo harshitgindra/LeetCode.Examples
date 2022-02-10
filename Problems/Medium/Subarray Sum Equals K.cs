691c055badd2a638f4f31d2d610bcce688d45804
Harshit Gindra <harshitgindra@gmail.com>
2022-02-09 20:24:42 -0800

    https://leetcode.com/problems/subarray-sum-equals-k/

---
+++ Problems/Medium/Subarray Sum Equals K.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace Leetcode.Problems.Medium
+{
+    /// <summary>
+    /// https://leetcode.com/problems/subarray-sum-equals-k/
+    /// </summary>
+    public class Subarray_Sum_Equals_K
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            var dict = new Dictionary<int, int>();
+            dict.Add(0, 1);
+            
+            int ret = 0, sum = 0;
+            foreach (var n in nums)
+            {
+                sum += n;
+                if (dict.ContainsKey(sum - k))
+                {
+                    ret += dict[sum - k];
+                }
+
+                if (!dict.ContainsKey(sum))
+                {
+                    dict.Add(sum, 0);
+
+                }
+                
+                dict[sum]++;
+            }
+
+            return ret;
+        }
+    }
+}
