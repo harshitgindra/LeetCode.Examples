9610fe20d6726b5623960ce5bb810c52af63bc01
Harshit Gindra <harshitgindra@gmail.com>
2022-02-15 01:49:58 -0800

    https://leetcode.com/problems/single-number

--- Problems/Easy/SingleNumber.cs
+++ Problems/Easy/SingleNumber.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace LeetCode
 {
+    /// <summary>
+    /// https://leetcode.com/problems/single-number/
+    /// </summary>
     public class SingleNumberTest
     {
         public SingleNumberTest()
@@ -14,25 +17,26 @@ namespace LeetCode
             Console.WriteLine("Single Number Test");
             Console.WriteLine("----------------------------------------------------------");
         }
-
-
-
-        public int SingleNumber(int[] nums)
-        {
-            HashSet<int> i = new HashSet<int>();
-            foreach (var num in nums)
-            {
-                if (i.Contains(num))
-                {
-                    i.Remove(num);
-                }
-                else
-                {
-                    i.Add(num);
-                }
+        public int SingleNumber(int[] nums) {
+        
+            if(nums == null || nums.Length %2 == 0){
+                return 0;
             }
-
-            return i.First();
+        
+            Array.Sort(nums);
+        
+            for(int i = 0; i <= nums.Length-1; i=i+2)
+            {
+                if(i == nums.Length - 1){
+                    return nums[i];
+                }
+                else if(nums[i] != nums[i+1]){
+                    return nums[i];
+                }
+            
+            }
+        
+            return 0;
         }
     }
 }
