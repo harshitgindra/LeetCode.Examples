9691ab8e8296d43ab54c4302e72959b53b49830e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2022-11-29 22:02:34 -0800

    https://leetcode.com/problems/unique-number-of-occurrences/

---
+++ Problems/Easy/Unique Number of Occurrences.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Leetcode.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/unique-number-of-occurrences/
+    /// </summary>
+    public class Unique_Number_of_Occurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            bool[] counts = new bool[1000];
+            var dict = new Dictionary<int, int>();
+            foreach (var item in arr)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+
+                dict[item]++;
+            }
+
+            foreach (var item in dict)
+            {
+                if (counts[item.Value])
+                {
+                    return false;
+                }
+
+                counts[item.Value] = true;
+            }
+
+            return true;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/unique-number-of-occurrences/")]
+        [Category("Easy")]
+        [Category("LeetCode")]
+        [Category("Unique Number of Occurrences")]
+        [TestCaseSource("Input")]
+        public void Test1((bool Output, int[] Input) item)
+        {
+            var response = UniqueOccurrences(item.Input);
+            Assert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(bool Output, int[] Input)> Input
+        {
+            get
+            {
+                return new List<(bool Output, int[] Input)>()
+                {
+                    (true, new int[] {1, 2, 2, 1, 1, 3}),
+                    (true, new int[] {-3,0,1,-3,1,1,1,-3,10,0}),
+                    (false, new int[] {1,2}),
+                };
+            }
+        }
+    }
+}
