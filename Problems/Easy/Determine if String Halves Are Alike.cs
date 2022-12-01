51aa57d499866ed3a23dd6e643266b92e00598f4
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2022-11-30 22:40:50 -0800

    https://leetcode.com/problems/determine-if-string-halves-are-alike/

---
+++ Problems/Easy/Determine if String Halves Are Alike.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+
+namespace Leetcode.Problems.Easy
+{
+    /// <summary>
+    /// https://leetcode.com/problems/determine-if-string-halves-are-alike/
+    /// </summary>
+    public class Determine_if_String_Halves_Are_Alike
+    {
+        public bool HalvesAreAlike(string s)
+        {
+            int sIndex = 0;
+            int eIndex = s.Length - 1;
+            HashSet<char> vowels = new HashSet<char>()
+            {
+                'a', 'e', 'i', 'o', 'u',
+                'A', 'E', 'I', 'O', 'U',
+            };
+
+            int count = 0;
+            
+            while (sIndex < eIndex)
+            {
+                if (vowels.Contains(s[sIndex]))
+                {
+                    count++;
+                }
+                
+                if (vowels.Contains(s[eIndex]))
+                {
+                    count--;
+                }
+
+                sIndex++;
+                eIndex--;
+            }
+
+            return count == 0;
+        }
+    }
+}
