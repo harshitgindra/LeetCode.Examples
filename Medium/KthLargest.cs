9f6c0fac191b1470ea53fa05141aea41859eabda
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2024-08-16 22:06:17 -0700

    https://leetcode.com/problems/kth-largest-element-in-a-stream/

---
+++ Medium/KthLargest.cs
@@ -0,0 +1,22 @@
+namespace Medium;
+/// <summary>
+/// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+/// </summary>
+public class KthLargest
+{
+    private readonly PriorityQueue<int, int> priorityQueue = new();
+    private readonly int _k = 1;
+
+    public KthLargest(int k, int[] nums) {
+        _k = k;
+        foreach (var v in nums)
+            Add(v);
+    }
+    
+    public int Add(int val) {
+        priorityQueue.Enqueue(val, val);
+        if (priorityQueue.Count > _k)
+            priorityQueue.Dequeue();
+        return priorityQueue.Peek();
+    }
+}
