bd7dbe825f6559ebfaf346fa08b24516181e1afe
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-02-23 17:03:20 -0800

    https://leetcode.com/problems/valid-palindrome/

---
+++ EasyProblems/PascalTriangleII.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.EasyProblems
+{
+    public class PascalTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            if (rowIndex == 1)
+            {
+                return new int[] { 1 };
+            }
+            
+            int[] prev = new int[2]{1,1};
+            if (rowIndex == 2)
+            {
+                return prev;
+            }
+            
+            for (int i = 2; i < rowIndex; i++)
+            {
+                var current = Iterate(prev);
+                prev = current;
+            }
+
+            return prev;
+        }
+
+        private int[] Iterate(int[] prev)
+        {
+            int[] current = new int[prev.Length + 1];
+            
+            current[0] = prev[0];
+            current[prev.Length] = prev[prev.Length - 1];
+
+            for (int i = 1; i < prev.Length ; i++)
+            {
+                current[i] = prev[i - 1] + prev[i];
+            }
+
+            return current;
+        }
+    }
+}
---
+++ EasyProblems/ValidPalindrome.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework.Legacy;
+
+namespace LeetCode
+{
+    public class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (s == null || s.Length < 2)
+            {
+                return true;
+            }
+            else
+            {
+                int sIndex = 0;
+                int eIndex = s.Length - 1;
+                while (sIndex < s.Length)
+                {
+                    if (!char.IsLetterOrDigit(s[sIndex]))
+                    {
+                        sIndex++;
+                    } else if (!char.IsLetterOrDigit(s[eIndex]))
+                    {
+                        eIndex--;
+                    }
+                    else
+                    {
+                        char c1 = s[sIndex];
+                        char c2 = s[eIndex];
+                        if (char.ToUpper(s[sIndex]) != char.ToUpper(s[eIndex]))
+                        {
+                            return false;
+                        }
+
+                        sIndex++;
+                        eIndex--;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/valid-palindrome/")]
+        [Category("Easy")]
+        [Category("LeetCode")]
+        [Category("Valid Palindrome")]
+        [TestCaseSource("Input")]
+        public void Test1((bool Output, string Input) item)
+        {
+            var response = IsPalindrome(item.Input);
+            ClassicAssert.AreEqual(item.Output, response);
+        }
+
+        public static IEnumerable<(bool Output, string Input)> Input
+        {
+            get
+            {
+                return new List<(bool Output, string Input)>()
+                {
+                    (false, "race a car"),
+                    (true, "A man, a plan, a canal: Panama"),
+                };
+            }
+        }
+    }
+}
