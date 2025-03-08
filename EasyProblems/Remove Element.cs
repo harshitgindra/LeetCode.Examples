5eb35e3f3592d12fe62a5dbd2654179ae53181a1
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-08 15:19:11 -0800

    https://leetcode.com/problems/remove-element/

--- EasyProblems/Remove Element.cs
+++ EasyProblems/Remove Element.cs
@@ -6,50 +6,35 @@ namespace LeetCode.EasyProblems
     {
         public int RemoveElement(int[] nums, int val)
         {
-            if (nums != null && nums.Length > 0)
-            {
-                int returnValue = nums.Length;
+            int j = 0;
 
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    if (nums[i] == val)
-                    {
-                        nums[i] = int.MaxValue;
-                        returnValue--;
-                    }
+            for (int i = 0; i < nums.Length; i++) {
+                if (nums[i] != val) {
+                    nums[j] = nums[i];
+                    j++;
                 }
+            }
 
-                Array.Sort(nums);
-                return returnValue;
-            }
-            else
-            {
-                return 0;
-            }
+            return j;
         }
 
         [Test(Description = "https://leetcode.com/problems/remove-element/")]
         [Category("Easy")]
         [Category("LeetCode")]
         [Category("Remove Element")]
-        [TestCaseSource("Input")]
+        [TestCaseSource(nameof(Input))]
         public void Test1((int Output, (int[], int) Input) item)
         {
             var response = RemoveElement(item.Input.Item1, item.Input.Item2);
-            ClassicAssert.AreEqual(item.Output, response);
+            Assert.That(response, Is.EqualTo(item.Output));
         }
 
-        public static IEnumerable<(int Output, (int[], int) Input)> Input
-        {
-            get
+        public static IEnumerable<(int Output, (int[], int) Input)> Input =>
+            new List<(int Output, (int[], int) Input)>()
             {
-                return new List<(int Output, (int[], int) Input)>()
-                {
 
-                    (2, (new int[] {3,2,2,3}, 3)),
-                    (5, (new int[] {0,1,2,2,3,0,4,2}, 2)),
-                };
-            }
-        }
+                (2, ([3,2,2,3], 3)),
+                (5, ([0,1,2,2,3,0,4,2], 2)),
+            };
     }
 }
