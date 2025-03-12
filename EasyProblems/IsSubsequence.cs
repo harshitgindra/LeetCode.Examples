de5f62b0c5481010dae510ad8537ef043d091d2e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-12 12:46:15 -0700

    https://leetcode.com/problems/is-subsequence

---
+++ EasyProblems/IsSubsequence.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework.Legacy;
+
+namespace LeetCode.EasyProblems;
+
+public class IsSubsequenceSolution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.Length;
+    }
+    
+    [Test(Description = "https://leetcode.com/problems/is-subsequence/")]
+    [Category("Easy")]
+    [Category("LeetCode")]
+    [Category("Is Subsequence")]
+    [TestCaseSource(nameof(Input))]
+    public void Test1((bool Output, (string, string) Input) item)
+    {
+        var response = IsSubsequence(item.Input.Item1, item.Input.Item2);
+        ClassicAssert.IsTrue(response);
+    }
+
+    public static IEnumerable<(bool Output, (string, string) Input)> Input =>
+        new List<(bool Output, (string, string) Input)>()
+        {
+
+            (true, ("abc", "ahbgdc")),
+        };
+}
