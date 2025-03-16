dd0f6e5eca2cbbe63e3fc39a6aa7ae7252544a0a
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-03-16 15:18:33 -0700

    https://leetcode.com/problems/add-digits/

--- EasyProblems/Add Digits.cs
+++ EasyProblems/Add Digits.cs
@@ -5,36 +5,11 @@
     /// </summary>
     class Add_Digits
     {
-        private IDictionary<char, int> dict = new Dictionary<char, int>
-        {
-            {'1',1 },
-            {'2',2 },
-            {'3',3 },
-            {'4',4 },
-            {'5',5 },
-            {'6',6 },
-            {'7',7 },
-            {'8',8 },
-            {'9',9 },
-            {'0',0 },
-        };
-
-        public int AddDigits(int num)
-        {
-            if (num > 9)
-            {
-                var str = num.ToString();
-                var sum = 0;
-                foreach (var item in str)
-                {
-                    sum += dict[item];
-                }
-                return AddDigits(sum);
-            }
-            else
-            {
-                return num;
-            }
+        public int AddDigits(int num) {
+            if(num == 0)
+                return 0;
+            int returnValue = num % 9;
+            return returnValue == 0 ? 9 : returnValue;
         }
     }
 }
