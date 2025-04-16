83c9faa95a8963173dea4f475d6dd7099fdb74e2
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-04-15 23:17:38 -0700

    https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/

--- EasyProblems/Two Sum II - Input array is sorted.cs
+++ EasyProblems/Two Sum II - Input array is sorted.cs
@@ -1,25 +1,37 @@
 ﻿namespace LeetCode.EasyProblems
 {
-    class Two_Sum_II___Input_array_is_sorted
+    class TwoSumIiInputArrayIsSorted
     {
         public int[] TwoSum(int[] numbers, int target)
         {
-            int[] ret = new int[2];
-            for (int i = 0; i < numbers.Length - 1; i++)
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
             {
-                var diff = target - numbers[i];
-                for (int j = i + 1; j < numbers.Length; j++)
-                {
-                    if (numbers[j] == diff)
-                    {
-                        ret[0] = i+1;
-                        ret[1] = j+1;
-                        return ret;
-                    }
-                }
+                int sum = numbers[left] + numbers[right];
+                if (sum == target) break;
+                if (sum < target) left++;
+                if (sum > target) right--;
             }
 
-            return ret;
+            return [left + 1, right + 1];
         }
+
+        [Test(Description = "https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/")]
+        [Category("Medium")]
+        [Category("LeetCode")]
+        [Category("Two Sum II - Input array is sorted")]
+        [TestCaseSource(nameof(Input))]
+        public void Test1((int[] Output, (int[] numbers, int target) Input) item)
+        {
+            var response = TwoSum(item.Input.numbers, item.Input.target);
+            Assert.That(item.Output, Is.EqualTo(response));
+        }
+
+        public static IEnumerable<(int[] Output, (int[] numbers, int target) Input)> Input =>
+            new List<(int[] Output, (int[] numbers, int target) Input)>()
+            {
+                ([1, 2], ([2, 7, 11, 15], 9)),
+            };
     }
 }
