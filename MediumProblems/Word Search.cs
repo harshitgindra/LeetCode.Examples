8ed70985fb341d5583bb476af1fa517c5762044e
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-04-25 17:44:56 -0700

    https://leetcode.com/problems/word-search/

--- MediumProblems/Word Search.cs
+++ MediumProblems/Word Search.cs
@@ -1,102 +1,90 @@
 namespace LeetCode.MediumProblems
 {
-    public class Word_Search
+    public class WordSearch
     {
+        // Time: O(m × n × 3^k)
+        // Space: O(k)
         public bool Exist(char[][] board, string word)
         {
-            int iMax = board.Length;
-            int jMax = board[0].Length;
-
-            for (int i = 0; i < iMax; i++)
-            {
-                for (int j = 0; j < jMax; j++)
-                {
-                    if (board[i][j] == word[0])
-                    {
-                        if (word.Length == 1)
-                        {
-                            return true;
-                        }
-                        else if (StartSearching(i, j, iMax, jMax, 0, board, word))
-                        {
-                            return true;
-                        }
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        private bool StartSearching(int i, int j, int iMax, int jMax, int current, char[][] board, string word)
-        {
-            if (current == word.Length)
-            {
-                return true;
-            }
-            else if (i < 0 || i >= iMax || j < 0 || j >= jMax || board[i][j] == '*')
-            {
+            // Check for empty board or empty word
+            if (board == null || board.Length == 0 || string.IsNullOrEmpty(word))
                 return false;
-            }
-            else
+
+            int rows = board.Length;
+            int cols = board[0].Length;
+
+            // Iterate through each cell in the board
+            for (int i = 0; i < rows; i++)
             {
-                if (board[i][j] == word[current])
+                for (int j = 0; j < cols; j++)
                 {
-                    board[i][j] = '*';
-                    if (StartSearching(i + 1, j, iMax, jMax, current + 1, board, word))
-                    {
+                    // Start DFS from cells matching the first character
+                    if (board[i][j] == word[0] && Dfs(board, i, j, word, 0))
                         return true;
-                    }
-
-                    if (StartSearching(i - 1, j, iMax, jMax, current + 1, board, word))
-                    {
-                        return true;
-                    }
-
-                    if (StartSearching(i, j + 1, iMax, jMax, current + 1, board, word))
-                    {
-                        return true;
-                    }
-
-                    if (StartSearching(i, j - 1, iMax, jMax, current + 1, board, word))
-                    {
-                        return true;
-                    }
-
-                    board[i][j] = word[current];
                 }
             }
 
             return false;
         }
 
-        [Test(Description = "https://leetcode.com/problems/maximal-rectangle/")]
+        private bool Dfs(char[][] board, int row, int col, string word, int index)
+        {
+            // Base case: entire word found
+            if (index == word.Length - 1)
+                return true;
+
+            // Temporarily mark current cell as visited
+            char original = board[row][col];
+            board[row][col] = '#'; // Using '#' as visited marker
+
+            // Define directions: up, right, down, left
+            int[] dirs = { -1, 0, 1, 0, -1 };
+
+            // Explore all four directions
+            for (int d = 0; d < 4; d++)
+            {
+                int newRow = row + dirs[d];
+                int newCol = col + dirs[d + 1];
+
+                // Check boundaries and character match
+                if (newRow >= 0 && newRow < board.Length &&
+                    newCol >= 0 && newCol < board[0].Length &&
+                    board[newRow][newCol] == word[index + 1])
+                {
+                    // Recursive DFS call for next character
+                    if (Dfs(board, newRow, newCol, word, index + 1))
+                        return true;
+                }
+            }
+
+            // Backtrack: restore original character
+            board[row][col] = original;
+            return false;
+        }
+
+        [Test(Description = "https://leetcode.com/problems/word-search/")]
         [Category("Medium")]
         [Category("LeetCode")]
-        [Category("maximal rectangle")]
+        [Category("Word Search")]
         [TestCaseSource(nameof(Input))]
-        public void Test1((int Output, char[][] Input) item)
+        public void Test1((bool Output, (char[][], string) Input) item)
         {
-            var response = Exist(item.Input, "AA");
-            //ClassicAssert.AreEqual(item.Output, response);
+            var response = Exist(item.Input.Item1, item.Input.Item2);
+            Assert.That(response, Is.EqualTo(item.Output));
         }
 
-        public static IEnumerable<(int Output, char[][] Input)> Input
+        public static IEnumerable<(bool Output, (char[][], string) Input)> Input
         {
             get
             {
-                return new List<(int Output, char[][] Input)>()
+                return new List<(bool Output, (char[][], string) Input)>()
                 {
-                    (4, new char[][]
+                    (true, (new char[][]
                     {
-                        new char[] {'A','A'},
-                    }),
-                    (4, new char[][]
-                    {
-                        new char[] {'A', 'B', 'C', 'E'},
-                        new char[] {'S', 'F', 'C', 'S'},
-                        new char[] {'A', 'D', 'E', 'E'},
-                    }),
+                        new char[] { 'A', 'B', 'C', 'E' },
+                        new char[] { 'S', 'F', 'C', 'S' },
+                        new char[] { 'A', 'D', 'E', 'E' },
+                    }, "ABCCED")),
                 };
             }
         }
