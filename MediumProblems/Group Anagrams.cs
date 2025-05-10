cb59e039e0614888a754587a400315e7e3a76e7f
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-05-10 10:44:50 -0700

    https://leetcode.com/problems/group-anagrams/

--- MediumProblems/Group Anagrams.cs
+++ MediumProblems/Group Anagrams.cs
@@ -1,30 +1,80 @@
-﻿namespace LeetCode.MediumProblems
+﻿using System.Text;
+
+namespace LeetCode.MediumProblems
 {
-    class Group_Anagrams
+    class GroupAnagramsSolution
     {
-        public IList<IList<string>> GroupAnagrams(string[] strs)
+        public IList<IList<string>> GroupAnagrams(string[] strs) 
         {
-            IDictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
-            for (int i = 0; i < strs.Length; i++)
+            // Dictionary to hold groups of anagrams
+            Dictionary<string, List<string>> anagramGroups = new Dictionary<string, List<string>>();
+        
+            foreach (string str in strs)
             {
-                var o = string.Join("", strs[i].OrderBy(x => x));
-                if (dict.ContainsKey(o))
+                string key = GenerateAnagramKey(str);
+            
+                // Create new group or add to existing
+                if (!anagramGroups.TryGetValue(key, out List<string> group))
                 {
-                    dict[o].Add(strs[i]);
+                    group = new List<string>();
+                    anagramGroups[key] = group;
                 }
-                else
-                {
-                    dict.Add(o, new List<string>() { strs[i] });
-                }
+                group.Add(str);
             }
-
-            IList<IList<string>> ret = new List<IList<string>>();
-            foreach (var item in dict)
-            {
-                ret.Add(item.Value.ToList());
-            }
-
-            return ret;
+        
+            return new List<IList<string>>(anagramGroups.Values);
         }
+    
+        private string GenerateAnagramKey(string s)
+        {
+            int[] charCounts = new int[26];
+            // Count character frequencies
+            foreach (char c in s)
+            {
+                charCounts[c - 'a']++;
+            }
+        
+            // Build unique key from counts
+            StringBuilder keyBuilder = new StringBuilder();
+            foreach (int count in charCounts)
+            {
+                keyBuilder.Append(count).Append('#');
+            }
+        
+            return keyBuilder.ToString();
+        }
+        
+        [Test(Description = "https://leetcode.com/problems/group-anagrams/")]
+        [Category("Medium")]
+        [Category("LeetCode")]
+        [Category("Group Anagrams")]
+        [TestCaseSource(nameof(Input))]
+        public void Test1((IList<IList<string>> Output, string[] Input) item)
+        {
+            var response = GroupAnagrams(item.Input);
+            // Normalize both actual and expected results
+            var normalizedResponse = NormalizeAnagramGroups(response);
+            var normalizedExpected = NormalizeAnagramGroups(item.Output);
+    
+            Assert.That(normalizedResponse, Is.EqualTo(normalizedExpected));
+        }
+        
+        private IList<List<string>> NormalizeAnagramGroups(IList<IList<string>> groups)
+        {
+            return groups
+                .Select(group => 
+                        group.OrderBy(s => s).ToList() // Sort individual anagrams
+                )
+                .OrderBy(group => 
+                        group.FirstOrDefault() // Sort groups by first element
+                )
+                .ToList();
+        }
+
+        public static IEnumerable<(IList<IList<string>> Output, string[] Input)> Input =>
+            new List<(IList<IList<string>> Output, string[] Input)>()
+            {
+                ( [["bat"],["nat","tan"],["ate","eat","tea"]], ["eat","tea","tan","ate","nat","bat"]),
+            };
     }
 }
