e26f7d8d05072d88dfc52ec791453e07e80348d4
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-05-19 18:32:10 -0700

    https://leetcode.com/problems/course-schedule

--- MediumProblems/Course Schedule.cs
+++ MediumProblems/Course Schedule.cs
@@ -1,78 +1,80 @@
 namespace LeetCode.MediumProblems
 {
-    public class Course_Schedule
+    /// <summary>
+    /// https://leetcode.com/problems/course-schedule
+    /// </summary>
+    public class CourseSchedule
     {
         public bool CanFinish(int numCourses, int[][] prerequisites)
         {
-            if (numCourses < 2)
+            // Create adjacency list representation of the graph
+            List<int>[] graph = new List<int>[numCourses];
+            for (int i = 0; i < numCourses; i++)
             {
-                return true;
+                graph[i] = new List<int>();
             }
-            else
-            {
-                int[] arry = new int[numCourses];
 
-                for (var i = 0; i < numCourses; i++)
+            // Build the graph: for each prerequisite pair [a,b], add an edge from b to a
+            // (meaning b must be taken before a)
+            foreach (var prereq in prerequisites)
+            {
+                int course = prereq[0];
+                int prerequisite = prereq[1];
+                graph[prerequisite].Add(course);
+            }
+
+            // Array to track visited nodes in current DFS path (for cycle detection)
+            bool[] visited = new bool[numCourses];
+
+            // Array to track nodes that have been fully processed
+            bool[] completed = new bool[numCourses];
+
+            // Check each course for cycles
+            for (int i = 0; i < numCourses; i++)
+            {
+                if (!completed[i] && HasCycle(graph, i, visited, completed))
                 {
-                    arry[i] = 2;
-                    if (!Check(arry, i, prerequisites))
-                    {
-                        return false;
-                    }
-
-                    arry[i] = 1;
+                    return false; // Cycle detected, cannot finish all courses
                 }
-
-                return true;
             }
+
+            return true; // No cycles found, can finish all courses
         }
 
-
-        private bool Check(int[] arry, int currCourse, int[][] prerequisites)
+        // DFS to detect cycles in the graph
+        private bool HasCycle(List<int>[] graph, int course, bool[] visited, bool[] completed)
         {
-            foreach (var item in prerequisites.Where(x => x[0] == currCourse))
+            // If this node is already in our current path, we found a cycle
+            if (visited[course])
             {
-                if (arry[item[1]] == 1)
-                {
-                    return true;
-                }
-                else if (arry[item[1]] == 2)
-                {
-                    return false;
-                }
-                else
-                {
-                    arry[item[1]] = 2;
-                    if (!Check(arry, item[1], prerequisites))
-                    {
-                        return false;
-                    }
+                return true;
+            }
 
-                    arry[item[1]] = 1;
+            // If we've already processed this node and found no cycles, skip it
+            if (completed[course])
+            {
+                return false;
+            }
+
+            // Mark the current node as visited in current path
+            visited[course] = true;
+
+            // Recursively check all adjacent nodes (courses that depend on this one)
+            foreach (int nextCourse in graph[course])
+            {
+                if (HasCycle(graph, nextCourse, visited, completed))
+                {
+                    return true; // Propagate cycle detection
                 }
             }
 
-            return true;
-        }
+            // Mark the current node as not visited in current path (backtracking)
+            visited[course] = false;
 
-        // private bool CheckPreReq(int course, int[][] dict, HashSet<int> pendingCourses)
-        // {
-        //     if (pendingCourses.Add(course))
-        //     {
-        //         foreach (var item in dict.Where(x => x[1] == course))
-        //         {
-        //             if (!CheckPreReq(item[0], dict, pendingCourses))
-        //             {
-        //                 return false;
-        //             }
-        //         }
-        //     }
-        //     else
-        //     {
-        //         return false;
-        //     }
-        //
-        //     return true;
-        // }
+            // Mark this node as fully processed (no cycles found)
+            completed[course] = true;
+
+            return false; // No cycles found
+        }
     }
 }
