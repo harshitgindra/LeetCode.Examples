ab80f73ca0ab8a8afc0f09dd902eb461838c7de6
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-05-23 11:42:22 -0700

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/

--- EasyProblems/Best Time to Buy and Sell Stock.cs
+++ EasyProblems/Best Time to Buy and Sell Stock.cs
@@ -1,26 +1,36 @@
 namespace LeetCode.EasyProblems
 {
-    /// <summary>
-    /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
-    /// </summary>
-    public class Best_Time_to_Buy_and_Sell_Stock
+    public class BestTimeToBuyAndSellStock
     {
         public int MaxProfit(int[] prices)
         {
             int profit = 0;
+            int bestPriceToBuy = prices[0];
 
-            if (prices != null && prices.Length > 1)
+            for (int i = 1; i < prices.Length; i++)
             {
-                int bestPriceToBuy = prices[0];
-
-                for (int i = 1; i < prices.Length; i++)
-                {
-                    profit = Math.Max(profit, prices[i] - bestPriceToBuy);
-                    bestPriceToBuy = Math.Min(bestPriceToBuy, prices[i]);
-                }
+                profit = Math.Max(profit, prices[i] - bestPriceToBuy);
+                bestPriceToBuy = Math.Min(bestPriceToBuy, prices[i]);
             }
 
             return profit;
         }
+        
+        [Test(Description = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock/")]
+        [Category("Easy")]
+        [Category("LeetCode")]
+        [Category("Best Time to Buy and Sell Stock")]
+        [TestCaseSource(nameof(Input))]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = MaxProfit(item.Input);
+            Assert.That(response, Is.EqualTo(item.Output));
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input =>
+            new List<(int Output, int[] Input)>()
+            {
+                (5, ( [7,1,5,3,6,4])),
+            };
     }
 }
---
+++ MediumProblems/Best Time to Buy and Sell Stock II.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.MediumProblems
+{
+    class BestTimeToBuyAndSellStockIi
+    {
+        public int MaxProfit(int[] prices) {
+        
+            int profit = 0;
+        
+            for(int i = 1; i < prices.Length; i++)
+            {
+                if(prices[i] > prices[i - 1])
+                    profit += prices[i] - prices[i - 1];
+            }
+        
+            return profit;
+        
+        }
+
+        [Test(Description = "https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/")]
+        [Category("Medium")]
+        [Category("LeetCode")]
+        [Category("Best Time to Buy and Sell Stock II")]
+        [TestCaseSource(nameof(Input))]
+        public void Test1((int Output, int[] Input) item)
+        {
+            var response = MaxProfit(item.Input);
+            Assert.That(response, Is.EqualTo(item.Output));
+        }
+
+        public static IEnumerable<(int Output, int[] Input)> Input =>
+            new List<(int Output, int[] Input)>()
+            {
+
+                (8, ( [3,3,5,0,0,3,1,4])),
+                (7, ( [7,1,5,3,6,4])),
+                (4, ( [1,2,3,4,5])),
+                (0, ( [7,6,4,3,1])),
+            };
+    }
+}
