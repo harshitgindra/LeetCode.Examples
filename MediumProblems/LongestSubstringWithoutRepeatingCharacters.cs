79b7438cb47d4df536126507e0dcff8d8216e412
Harshit Gindra <9598218+harshitgindra@users.noreply.github.com>
2025-05-27 18:09:50 -0700

    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- MediumProblems/LongestSubstringWithoutRepeatingCharacters.cs
+++ MediumProblems/LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,35 +1,33 @@
 ﻿namespace LeetCode.MediumProblems
 {
+    /// <summary>
+    /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    /// </summary>
     public class LongestSubstringWithoutRepeatingCharacters
     {
-
-
         public int LengthOfLongestSubstring(string s)
         {
-            int maxLength = 0;
+            HashSet<char> uniqueChars = new HashSet<char>(); // Track unique characters in current window
+            int windowStart = 0; // Left boundary of window
+            int maxLength = 0; // Result: length of longest substring
 
-            for (var i = 0; i < s.Length; i++)
+            // Slide windowEnd across the string
+            foreach (int windowEnd in Enumerable.Range(0, s.Length))
             {
-                for (var j = maxLength; j <= s.Length - i; j++)
+                // If current character is already in set, move windowStart right
+                while (uniqueChars.Contains(s[windowEnd]))
                 {
-                    if (maxLength < j)
-                    {
-                        string subStr = s.Substring(i, j);
-                        if (subStr.Length == subStr.Distinct().Count())
-                        {
-                            maxLength = j;
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
+                    uniqueChars.Remove(s[windowStart]);
+                    windowStart++;
                 }
+
+                // Add current character to set
+                uniqueChars.Add(s[windowEnd]);
+                // Update maxLength if current window is larger
+                maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
             }
 
             return maxLength;
         }
-
-      
     }
 }
